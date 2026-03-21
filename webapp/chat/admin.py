462f05ee71997621bf85db8dafea2b01c6b735f6
syagmurtugtag <selinyagmur02@hotmail.com>
2026-03-21 15:48:39 +0300

    Add Conversation to Django admin

--- webapp/chat/admin.py
+++ webapp/chat/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import KnowledgeBase, ChatMessage
+from .models import KnowledgeBase, ChatMessage, Conversation
 
 @admin.register(KnowledgeBase)
 class KnowledgeBaseAdmin(admin.ModelAdmin):
@@ -11,3 +11,8 @@ class KnowledgeBaseAdmin(admin.ModelAdmin):
 class ChatMessageAdmin(admin.ModelAdmin):
     list_display = ['question', 'created_at']
     readonly_fields = ['question', 'answer', 'created_at']
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    search_fields = ['title']
